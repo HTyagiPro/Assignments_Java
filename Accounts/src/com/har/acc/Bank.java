f60bb61d1d6182c145c87af3835b74e0dfd5c879
HTyagiPro <138974061+HTyagiPro@users.noreply.github.com>
2023-07-16 21:48:07 +0530

    Update Bank.java

--- Accounts/src/com/har/acc/Bank.java
+++ Accounts/src/com/har/acc/Bank.java
@@ -4,6 +4,7 @@ public class Bank {
 	private String bankName;
 	private String branchName;
 	private Account[] acc;
+	public int noOfTransaction = 0;
 	
 	public Bank() {	}
 	
@@ -20,19 +21,22 @@ public class Bank {
 				acc[i] = a;
 				added = true;
 			}
-			
 		}
 		if(!added)
 			throw new InsufficientBalanceException("Account Book Full!!!");
 	}
 	
-	public String transactionForBank(char ttype,Account a,double amount) {
+	public String transactionForBank(char ttype,Account a,double amount) throws InsufficientBalanceException {
+		if(noOfTransaction >= ((SavingAccount) a).getNoOfTransactionsPerDay())
+			throw new InsufficientBalanceException("Transaction Limit Exceeded!!");
 		switch(ttype) {
 		case 'w':
 			Transaction.withdraw(a, amount);
+			noOfTransaction += 1;
 			return "Transaction Successful\nAmount Withdraw: "+amount+" /-";
 		case 'd':
 			Transaction.deposite(a, amount);
+			noOfTransaction += 1;
 			return "Transaction Successful\n Amount Deposited: "+amount+" /-";
 		default:
 			throw new InsufficientBalanceException("Invalid Transaction!!!");
@@ -41,16 +45,22 @@ public class Bank {
 	}
 	
 	public void showAccounts() {
-		System.out.println("---------------Bank Details---------------");
-		System.out.println("Account Number: "+ this.getBankName());
-		System.out.println("Account Holder's Name: "+ this.getBranchName());
+		System.out.println("---------------Bank Details--------------");
+		System.out.println("Bank Name: "+ this.getBankName());
+		System.out.println("Branch Name: "+ this.getBranchName());
 		System.out.println("-----------------------------------------");
 		System.out.println("-------------Account Details-------------");
 		for(int i=0; i<acc.length && acc[i] != null;i++) {
 			System.out.println("Account Number: "+ acc[i].getAccountNo());
 			System.out.println("Account Holder's Name: "+ acc[i].getName());
 			System.out.println("Account Balance: "+ acc[i].getBalance());
-			System.out.println("Account Type: "+acc[i].getClass().getSimpleName() );
+			System.out.println("Account Type: "+acc[i].getClass().getSimpleName());
+			
+			if (acc[i].getClass().getSimpleName().equals("SavingAccount"))
+				System.out.println("Per-day Transactions Limit: "+((SavingAccount) acc[i]).getNoOfTransactionsPerDay());
+			else if  (acc[i].getClass().getSimpleName().equals("CurrentAccount"))
+				System.out.println("Overdraft Limit: "+((CurrentAccount) acc[i]).getOverDraftLimit());
+			
 			System.out.println("Date of Account Opening : " + acc[i].dateOfOpening.getDayOfMonth()+"-"+acc[i].dateOfOpening.getMonthValue()+"-"+acc[i].dateOfOpening.getYear());
 			System.out.println("-----------------------------------------");
 			
@@ -80,9 +90,5 @@ public class Bank {
 
 	public void setAccounts(Account[] acc) {
 		this.acc = acc;
-	}
-
-	
-	
-	
+	}	
 }
