4a2052b2c91d3634788f5b20f90ee5b51c38a0eb
HTyagiPro <138974061+HTyagiPro@users.noreply.github.com>
2023-07-16 21:04:01 +0530

    Update SavingAccount.java
    Transactions per day limit added.

--- Accounts/src/com/har/acc/SavingAccount.java
+++ Accounts/src/com/har/acc/SavingAccount.java
@@ -11,21 +11,19 @@ package com.har.acc;
 
 public class SavingAccount extends Account{
 	double minimumDepositAmount;
-	int noOfTransactionsPerDay;
+	static int noOfTransactionsPerDay;
 	Account a;
 	
-		
-	
-
 	public SavingAccount(Account a, int noOfTransactionsPerDay) {
 		super(a.getAccountNo(), a.getName(), a.getBalance(),a.getDateOfOpening(), a.getMinimumDepositAmount());
-		this.noOfTransactionsPerDay = noOfTransactionsPerDay;
+		SavingAccount.noOfTransactionsPerDay = noOfTransactionsPerDay;
 	}
 	public int getNoOfTransactionsPerDay() {
 		return noOfTransactionsPerDay;
 	}
 	public void setNoOfTransactionsPerDay(int noOfTransactionsPerDay) {
-		this.noOfTransactionsPerDay = noOfTransactionsPerDay;
+		SavingAccount.noOfTransactionsPerDay = noOfTransactionsPerDay;
 	}
 	
 }
+
