21d012da5575b3b340d4f91aa77462fac74cac56
HTyagiPro <138974061+HTyagiPro@users.noreply.github.com>
2023-07-19 12:12:45 +0530

    Create GenericStack.java
    GenericStack created day 18th July 2023

---
+++ GenericStack.java
@@ -0,0 +1,82 @@
+import java.time.LocalDate;
+
+class GenericStack<T>{
+	private T[] stack;
+	int toS;
+	
+	Stack(T[] stack){
+		this.stack = stack;
+		this.toS = -1;
+	}
+	
+	
+	public void push(T ele) {
+		if(!this.isFull()) {
+			this.toS++;
+			this.stack[this.toS] = ele;
+			
+		}else
+			System.out.println("Stack OverFlow!!!");
+	}
+	
+	public void pop() {
+		if(!this.isEmpty()) {
+			System.out.println(this.stack[this.toS]);
+			this.toS--;
+		}else
+			System.out.println("Stack Underflow!!!");
+	}
+	
+	public boolean isEmpty() {
+		if (this.toS ==-1)
+			return true;
+		else 
+			return false;
+	}
+	
+	public boolean isFull() {
+		if (stack.length == this.toS)
+			return true;
+		else 
+			return false;
+	}
+	public static void main(String[] args) {
+	/*	
+	GenericStack<Integer> s1 = new GenericStack<Integer>(new Integer[10]);
+		System.out.println(s1.isEmpty());
+		System.out.println(s1.isFull());
+		s1.push(0);
+		s1.push(9);
+		s1.push(6);
+		s1.push(11);
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		s1.pop();
+		System.out.println(s1.toS);
+		GenericStack<String> s2 = new GenericStack<String>(new String[10]);
+		s2.push("Hello");
+		s2.push("World");
+		s2.push("I");
+		s2.push("Am");
+		s2.push("Here");
+		s2.push("How");
+		s2.push("Are");
+		s2.push("You");
+		s2.pop();
+		s2.pop();
+		s2.pop();
+		s2.pop();
+		s2.pop();
+		System.out.println(s2.toS);
+		*/
+		
+		GenericStack<Generic_Employee> gemp = new GenericStack<Generic_Employee>(new Generic_Employee[10]);
+		gemp.push(new Generic_Employee(101, 62500, "Ajay Saxena", LocalDate.of(2002, 1, 30)));
+		gemp.push(new Generic_Employee(102, 62500, "Ajay Saxena", LocalDate.of(2003, 2, 22)));
+		gemp.push(new Generic_Employee(103, 62500, "Ajay Saxena", LocalDate.of(2004, 3, 30)));
+		gemp.push(new Generic_Employee(104, 62500, "Ajay Saxena", LocalDate.of(2005, 4, 30)));
+		gemp.pop();
+	}
+}
